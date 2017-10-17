9473c09f189cd304f6d7139bf68a7d196bafd371
Igor Kurek <igorkurek96@gmail.com>
2017-10-18 00:21:48 +0200

    [WIP] News

--- app/src/main/java/com/ikurek/pwr/news/NewsFragment.kt
+++ app/src/main/java/com/ikurek/pwr/news/NewsFragment.kt
@@ -8,32 +8,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-
 import com.ikurek.pwr.R
+import com.prof.rssparser.Article
+import com.prof.rssparser.Parser
 import kotlinx.android.synthetic.main.fragment_news.view.*
 
 
 class NewsFragment : Fragment() {
 
+    val TAG = "NewsFragment"
+    var listOfNews: ArrayList<NewsModel> = arrayListOf()
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         val view = inflater!!.inflate(R.layout.fragment_news, container, false)
 
         setupRecyclerView(view)
-
+        getNews(view)
         return view
     }
 
     private fun setupRecyclerView(view : View) {
-        var listOfNews : ArrayList<NewsModel> = arrayListOf()
-        listOfNews.add(NewsModel("test", "test", "test"))
-        listOfNews.add(NewsModel("test2", "test", "test"))
-        listOfNews.add(NewsModel("test3", "test", "test"))
-
         view.recycler_view_news_fragment.layoutManager = LinearLayoutManager(activity)
         view.recycler_view_news_fragment.adapter = NewsAdapter(listOfNews) {
             Toast.makeText(activity, "Clicked news: ${it.title}", Toast.LENGTH_LONG).show()
         }
+        view.swipe_refresh_news_fragment.setOnRefreshListener {
+            getNews(view)
+        }
+    }
+
+    private fun getNews(view: View) {
+
+        view.swipe_refresh_news_fragment.isRefreshing = true
+
+        if (!listOfNews.isEmpty()) {
+            listOfNews.clear()
+        }
+
+        val parser = Parser()
+        parser.execute("http://pwr.edu.pl/rss/pl/24.xml")
+        parser.onFinish(object : Parser.OnTaskCompleted {
+
+            override fun onTaskCompleted(list: ArrayList<Article>) {
+                for (article: Article in list) {
+                    listOfNews.add(NewsModel(article.title, article.link, article.pubDate.toString()))
+                }
+                view.recycler_view_news_fragment.adapter.notifyDataSetChanged()
+                view.swipe_refresh_news_fragment.isRefreshing = false
+            }
+
+            override fun onError() {
+                view.swipe_refresh_news_fragment.isRefreshing = false
+                Toast.makeText(activity, "Serwer PWr padł :-(", Toast.LENGTH_LONG).show()
+            }
+        })
+
+
     }
 
 }
